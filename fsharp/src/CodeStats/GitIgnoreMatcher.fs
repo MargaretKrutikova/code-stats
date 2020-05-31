accd884e96ea7dcefb84faedb49c4a7d1a2cc4df
Margarita Krutikova <margaretkru@gmail.com>
2020-05-31 22:23:05 +0200

    Refactor gitignore matcher, fix negating patterns

--- fsharp/src/CodeStats/GitIgnoreMatcher.fs
+++ fsharp/src/CodeStats/GitIgnoreMatcher.fs
@@ -5,23 +5,55 @@ open System
 open Microsoft.Extensions.FileSystemGlobbing
 open Utils
 
-type GitIgnorePatterns = GitIgnorePatterns of patterns : string seq
+type GitIgnorePatterns = GitIgnorePatterns of patterns : GitIgnorePattern seq
+and GitIgnorePattern = {
+  Pattern : string
+  IsNegating : bool
+}
 
-let private fileMatchesGitIgnore filePath glob =
-  let result = Matcher().AddInclude(glob).Match([filePath])
-  result.HasMatches
-
-let private getGitIgnorePath (directoryPath : string) : string =
-  Path.Combine [| directoryPath; ".gitignore" |]
+type private ValidGitIgnoreEntry = ValidGitIgnoreEntry of string
 
 let private isValidGitIgnoreEntry (entry : string) =
-  (entry.StartsWith("#") || String.IsNullOrWhiteSpace entry) |> not
+  (entry.StartsWith("#") || String.IsNullOrWhiteSpace entry || entry = "!") |> not
 
-let transformGitignoreToGlob lines =
-  lines 
-  |> Seq.map String.trim
-  |> Seq.filter isValidGitIgnoreEntry
-  // |> Seq.map (fun line -> )
+let private toValidGitIgnoreEntry (entry : string) =
+  let transformed = entry |> String.trim
+  if isValidGitIgnoreEntry transformed then 
+    ValidGitIgnoreEntry transformed |> Some
+  else 
+    None
+
+let private isNegatingPattern = String.startsWith "!"
+let private fromNegatingPattern (pattern : string) =
+  pattern.Substring(1, pattern.Length)
+
+let private toGitIgnorePattern (ValidGitIgnoreEntry entry) =
+  if isNegatingPattern entry then
+    { Pattern = fromNegatingPattern entry; IsNegating = true }
+  else 
+    { Pattern = entry; IsNegating = false }
+
+let getGitIgnorePath (directoryPath : string) : string =
+  Path.Combine [| directoryPath; ".gitignore" |]
+
+let transformToGitIgnorePatterns (lines : string seq) =
+    lines
+    |> Seq.map toValidGitIgnoreEntry
+    |> Seq.choose id
+    |> Seq.map toGitIgnorePattern
+    |> GitIgnorePatterns
+
+let shouldIgnoreFile (filePath : string) (GitIgnorePatterns patterns) =
+  let folder (matcher : Matcher) entry =
+    if entry.IsNegating then
+      matcher.AddExclude(entry.Pattern)
+    else
+      matcher.AddInclude(entry.Pattern)
+
+  patterns 
+  |> Seq.fold folder (Matcher())
+  |> Matcher.performMatch filePath
+  |> Matcher.hasMatches
 
 let readGitIgnorePatterns (folder : string) : Async<GitIgnorePatterns> =
   async {
@@ -31,12 +63,6 @@ let readGitIgnorePatterns (folder : string) : Async<GitIgnorePatterns> =
       return Seq.empty |> GitIgnorePatterns
     else
       let! entriesToIgnore = File.ReadAllLinesAsync gitignorePath |> Async.AwaitTask
-      return entriesToIgnore 
-        |> Seq.ofArray 
-        |> Seq.map String.trim
-        |> Seq.filter isValidGitIgnoreEntry
-        |> GitIgnorePatterns
+      return entriesToIgnore |> transformToGitIgnorePatterns
   }
 
-let shouldIgnore (GitIgnorePatterns patterns) (path : string) =
-  patterns |> Seq.exists (fileMatchesGitIgnore path)
--- fsharp/src/CodeStats/Utils.fs
+++ fsharp/src/CodeStats/Utils.fs
@@ -1,4 +1,5 @@
 namespace Utils
+open Microsoft.Extensions.FileSystemGlobbing
 
 module List =
   let split (predicate : 'a -> bool) (list : 'a list) : 'a list * 'a list =
@@ -12,3 +13,12 @@ module List =
 
 module String =
   let trim (str : string) = str.Trim()
+  let startsWith (value : string) (str : string) = str.StartsWith(value)
+
+module Matcher =
+  let performMatch (filePath : string) (matcher : Matcher) = 
+    matcher.Match(filePath)
+  
+  let hasMatches (result : PatternMatchingResult) =
+    result.HasMatches
+
