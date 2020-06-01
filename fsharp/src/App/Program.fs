1f2d78838612edb02ffa1690902b874f151865ff
Margarita Krutikova <margaretkru@gmail.com>
2020-06-01 23:10:49 +0200

    Sort lines

--- fsharp/src/App/Program.fs
+++ fsharp/src/App/Program.fs
@@ -4,7 +4,10 @@ open CodeStats
 let main argv =
     let codeStats = CodeStats.getStats dirPath |> Async.RunSynchronously
 
-    Map.iter (printfn "%s - %d") codeStats.FilesPerExtension
+    codeStats.FilesPerExtension 
+    |> Map.toSeq 
+    |> Seq.sortByDescending snd 
+    |> Seq.iter (fun (key, count) -> printfn "%s - %d" key count)
 
     printfn "Hello World from F#!"
     0 // return an integer exit code
