25bc914b9d3a1cb4867dc0054a0c79270b7eb8c5
Margarita Krutikova <margaretkru@gmail.com>
2020-10-06 16:38:09 +0200

    Validate command line args

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,22 +1,41 @@
 ﻿open CodeStats
+open System
+open System.IO
 
 type CommandLineOptions = {
     Path: string
 }
 
-let parseArgs args: CommandLineOptions option =
+type CmdOptionsValidationError = InvalidCmdArgs | PathEmpty | PathNotFound
+
+let parseArgs args: Result<CommandLineOptions, CmdOptionsValidationError> =
     match args with
-    | [path] -> { Path = path } |> Some
-    | _ -> None
+    | [path] -> 
+        if String.IsNullOrWhiteSpace path then
+            Error PathEmpty
+        else if Directory.Exists path |> not then 
+            Error PathNotFound
+        else 
+            { Path = path } |> Ok
+    | _ -> Error InvalidCmdArgs
+
+let printError error: unit =
+    match error with
+    | InvalidCmdArgs -> 
+        printfn "Incorrect arguments supplied"
+    | PathEmpty ->
+        printfn "Path cannot be empty"
+    | PathNotFound ->
+        printfn "Path is not found on disk"
 
 [<EntryPoint>]
 let main argv =
     let options = argv |> Seq.toList |> parseArgs
     match options with
-    | None ->
-        printfn "Incorrect arguments supplied"
+    | Error error ->
+        printError error 
         42
-    | Some { Path = path } ->
+    | Ok { Path = path } ->
         let codeStats = CodeStats.getStats path |> Async.RunSynchronously
 
         codeStats.FilesPerExtension 
